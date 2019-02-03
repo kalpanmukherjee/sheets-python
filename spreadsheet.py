cf8bd3c3d264ddc6deb9c5e447de0fe3d5d9e409
Kalpan Mukherjee <43297951+kalpanmukherjee@users.noreply.github.com>
2019-02-03 22:02:14 +0530

    initial commit

---
+++ spreadsheet.py
@@ -0,0 +1,12 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
+client = gspread.authorize(creds)
+
+sheet = client.open("Something").sheet1
+
+result = sheet.get_all_records()
+
+print(result)
